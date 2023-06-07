daf6d84294f5800fc83077f4da713bbf00d62a50
szklipy <92113332+szklipy@users.noreply.github.com>
2023-06-07 12:38:24 +0200

    utolso

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,29 +1,63 @@
 import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+import datetime
+import plotly.graph_objects as go
 
-# Assuming you have the sorted_results list from the previous code snippet
+st.write("""
 
-# Set the title and description of your web app
-st.title("Formula 1 2023 Season Predictions")
-st.write("Predicted Standings")
+""")
 
-# Display the predicted standings in a table
-table_data = {"Driver": [], "Predicted Standing": []}
-for driver in ypred_lr:
-    table_data["Driver: "].append(driver)
+link = '[Kaggle - Formula 1 World Championship (1950 - 2023)](https://www.kaggle.com/datasets/rohanrao/formula-1-world-championship-1950-2020)'
+st.markdown(link, unsafe_allow_html=True)
+
+st.sidebar.header('User Input Parameters')
+
+
+# Get user inputs
+def user_input_features():
+    driverId = st.sidebar.slider('driverId', 1, 900, 180)
+    number_x = st.sidebar.slider('number_x', 1, 250, 12)
+    totalRaces = st.sidebar.slider('totalRaces', 1, 100, 50)
+    winRate = st.sidebar.slider('winRate', 0, 5, 0)
+    age = st.sidebar.slider('age', 17, 50, 20)
+    fastestLapRate = st.sidebar.slider('fastestLapRate', 0, 5, 0)
+    qualifyingWinRate= st.sidebar.slider('qualifyingWinRate', 0, 5, 0)
     
+    data = {'driverId': driverId,
+            'number_x': number_x,
+            'totalRaces': totalRaces,
+            'winRate': winRate,
+            'age': age,
+            'fastestLapRate': fastestLapRate,
+            'qualifyingWinRate': qualifyingWinRate
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
 
-st.table(table_data)
+df = user_input_features()
 
-# Add any additional content or analysis you want to showcase
-# You can use Streamlit's various functions to display plots, charts, or explanations
+# Show user inputs
+st.subheader('User Input parameters')
+st.write(df)
 
-# For example, you can plot a bar chart of the predicted standings
-import matplotlib.pyplot as plt
+# Create Plotly plot
+columns = ['driverId', 'number_x', 'totalRaces', 'winRate', 'age','fastestLapRate','qualifyingWinRate']
 
-plt.bar(table_data["Driver"], table_data["Predicted Standing"])
-plt.xlabel("Driver")
-plt.ylabel("Predicted Standing")
-plt.xticks(rotation=45)
-st.pyplot(plt)
+# create a new DataFrame with the selected columns
+df_result = df[columns]
 
-# You can add more Streamlit components and visualization as per your requirements
+# Convert the first row of the DataFrame to a list
+y = df_result.values.tolist()[0]
+
+fig = go.Figure(data=go.Bar(x=columns, y=y), layout_title_text='Movie Features')
+st.plotly_chart(fig, use_container_width=True)
+
+model_final_pipe = pickle.load(open('model_final.pkl', 'rb'))
+
+prediction = model_final_pipe.predict(df)
+
+st.subheader('Predicted Movie Popularity')
+prediction = int(np.round(prediction, 0))
+st.title(prediction)
